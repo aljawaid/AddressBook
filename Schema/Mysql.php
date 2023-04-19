76f28592d799f81c06ef2872da317818e814d8ee
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-19 19:40:28 +0100

    Update Mysql.php

--- Schema/Mysql.php
+++ Schema/Mysql.php
@@ -8,30 +8,46 @@ const VERSION = 1;
 
 function version_1(PDO $pdo)
 {
-    $pdo->exec('CREATE TABLE IF NOT EXISTS contacts_items (
+    // CREATE DYNAMIC ITEMS FOR ONE GENERAL CONTACT
+    // ITEM TYPE WILL BE VALIDATED IN FRONTEND
+    $pdo->exec('CREATE TABLE IF NOT EXISTS address_book_contacts_items (
         `id` INT NOT NULL AUTO_INCREMENT,
         `item` VARCHAR(30) NOT NULL,
+        `item_type` TEXT NOT NULL,
         `position` INT NOT NULL,
         PRIMARY KEY(id)
-    ) ENGINE=InnoDB CHARSET=utf8');
+    ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
 
-    $pdo->exec("CREATE TABLE IF NOT EXISTS contacts_contact (
+    // SAVE VALUES OF DYNAMIC ITEMS FOR THE CONTACT
+    $pdo->exec("CREATE TABLE IF NOT EXISTS address_book_contacts_contact (
         `contacts_id` INT NOT NULL,
         `item_id` INT NOT NULL,
-        `value` VARCHAR(60) NOT NULL,
-        FOREIGN KEY(item_id) REFERENCES contacts_items(id) ON DELETE CASCADE,
+        `contact_item_value` TEXT NOT NULL,
+        `contact_status` TEXT NOT NULL,
+        `updated_by_user_id` INT NOT NULL,
+        `last_updated` INTEGER NOT NULL,
+        FOREIGN KEY(item_id) REFERENCES address_book_contacts_items(id) ON DELETE CASCADE,
         KEY(contacts_id)
-    ) ENGINE=InnoDB CHARSET=utf8");
+    ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci");
 
-    $pdo->exec("CREATE TABLE IF NOT EXISTS contacts_task_has_contact (
+    // CREATE LINKS TO TASKS WITH CONTACTS
+    $pdo->exec("CREATE TABLE IF NOT EXISTS address_book_contacts_task_has_contact (
         `id` INT NOT NULL AUTO_INCREMENT,
         `task_id` INT NOT NULL,
         `contacts_id` INT NOT NULL,
-        FOREIGN KEY(contacts_id) REFERENCES contacts_contact(contacts_id) ON DELETE CASCADE,
+        FOREIGN KEY(contacts_id) REFERENCES address_book_contacts_contact(contacts_id) ON DELETE CASCADE,
         FOREIGN KEY(task_id) REFERENCES tasks(id) ON DELETE CASCADE,
         PRIMARY KEY(id)
-    ) ENGINE=InnoDB CHARSET=utf8");
+    ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci");
 
-    $pdo->exec('INSERT INTO `contacts_items`(`item`, `position`) VALUES ("Name", 1)');
-    $pdo->exec('INSERT INTO `contacts_items`(`item`, `position`) VALUES ("Tel.", 2)');
+    // ADD DEFAULTS FOR CONTACT ITEMS
+    $pdo->exec('INSERT INTO `address_book_contacts_items`(`item`, `item_type`, `position`) VALUES ("Name", "text", 1)');
+    $pdo->exec('INSERT INTO `address_book_contacts_items`(`item`, `item_type`, `position`) VALUES ("Organisation", "text", 2)');
+    $pdo->exec('INSERT INTO `address_book_contacts_items`(`item`, `item_type`, `position`) VALUES ("Address", "address", 3)');
+    $pdo->exec('INSERT INTO `address_book_contacts_items`(`item`, `item_type`, `position`) VALUES ("Telephone", "telephone", 4)');
+    $pdo->exec('INSERT INTO `address_book_contacts_items`(`item`, `item_type`, `position`) VALUES ("Mobile", "telephone", 5)');
+    $pdo->exec('INSERT INTO `address_book_contacts_items`(`item`, `item_type`, `position`) VALUES ("Email", "email", 6)');
+    $pdo->exec('INSERT INTO `address_book_contacts_items`(`item`, `item_type`, `position`) VALUES ("Website", "website", 7)');
+    $pdo->exec('INSERT INTO `address_book_contacts_items`(`item`, `item_type`, `position`) VALUES ("Reference", "text", 8)');
+    $pdo->exec('INSERT INTO `address_book_contacts_items`(`item`, `item_type`, `position`) VALUES ("Note", "text", 9)');
 }
