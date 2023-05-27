34085d5634c92118a4dac41970eef8e86a561dd4
IJ <jawaid_imran@hotmail.com>
2023-05-27 14:47:55 +0100

    Update HTML

--- Template/project/contacts.php
+++ Template/project/contacts.php
@@ -1,32 +1,33 @@
 <?php $items = $this->ContactsHelper->getItems() ?>
-<div class="page-header">
-    <h2><?= t('Contacts') ?></h2>
-</div>
-
-<?php if (empty($contacts)): ?>
-    <p class="alert"><?= t('No contacts') ?></p>
-<?php else: ?>
-    <table class="table-small table-fixed">
-        <tr>
-            <th class="column-20"><?= (empty($items[0])) ? "" : $items[0]['item'] ?></th>
-            <th class="column-20"><?= (empty($items[1])) ? "" : $items[1]['item'] ?></th>
-            <th class="column-20"><?= (empty($items[2])) ? "" : $items[2]['item'] ?></th>
-            <th class="column-20"><?= (empty($items[3])) ? "" : $items[3]['item'] ?></th>
-            <th class="column-20"><?= t('Action') ?></th>
-        </tr>
-        <?php foreach ($contacts as $key => $value): ?>
-            <?php $values = $this->ContactsHelper->getContactByID($value['contacts_id']) ?>
-            <tr>
-                <td><?= (empty($values[1])) ? "" : $values[1]['value'] ?></td>
-                <td><?= (empty($values[2])) ? "" : $values[2]['value'] ?></td>
-                <td><?= (empty($values[3])) ? "" : $values[3]['value'] ?></td>
-                <td><?= (empty($values[4])) ? "" : $values[4]['value'] ?></td>
-                <td><?= $this->render('addressBook:project/menu', array('project' => $project, 'more' => !empty($values[5]), 'contacts_id' => $value['contacts_id'])) ?></td>
+<div class="">
+    <div class="page-header">
+        <h2 class=""><?= t('Contacts') ?></h2>
+    </div>
+    <?php if (empty($contacts)): ?>
+        <p class="alert"><?= t('No contacts') ?></p>
+    <?php else: ?>
+        <table class="table-small table-fixed">
+            <tr class="">
+                <th class="column-20"><?= (empty($items[0])) ? "" : $items[0]['item'] ?></th>
+                <th class="column-20"><?= (empty($items[1])) ? "" : $items[1]['item'] ?></th>
+                <th class="column-20"><?= (empty($items[2])) ? "" : $items[2]['item'] ?></th>
+                <th class="column-20"><?= (empty($items[3])) ? "" : $items[3]['item'] ?></th>
+                <th class="column-20"><?= t('Action') ?></th>
             </tr>
-        <?php endforeach ?>
-    </table>
-<?php endif ?>
+            <?php foreach ($contacts as $key => $value): ?>
+                <?php $values = $this->ContactsHelper->getContactByID($value['contacts_id']) ?>
+                <tr class="">
+                    <td class=""><?= (empty($values[1])) ? "" : $values[1]['value'] ?></td>
+                    <td class=""><?= (empty($values[2])) ? "" : $values[2]['value'] ?></td>
+                    <td class=""><?= (empty($values[3])) ? "" : $values[3]['value'] ?></td>
+                    <td class=""><?= (empty($values[4])) ? "" : $values[4]['value'] ?></td>
+                    <td class=""><?= $this->render('addressBook:project/menu', array('project' => $project, 'more' => !empty($values[5]), 'contacts_id' => $value['contacts_id'])) ?></td>
+                </tr>
+            <?php endforeach ?>
+        </table>
+    <?php endif ?>
 
-<?php if (isset($addNew) and $addNew): ?>
-    <?= $this->render('addressBook:contact/add', array('items' => $items, 'project_id' => $project['id'], 'values' => $values, 'errors' => $errors)) ?>
-<?php endif ?>
+    <?php if (isset($addNew) and $addNew): ?>
+        <?= $this->render('addressBook:contact/add', array('items' => $items, 'project_id' => $project['id'], 'values' => $values, 'errors' => $errors)) ?>
+    <?php endif ?>
+</div>
--- Template/project/menu.php
+++ Template/project/menu.php
@@ -1,17 +1,19 @@
 <div class="dropdown">
-    <a href="#" class="dropdown-menu dropdown-menu-link-icon"><i class="fa fa-cog fa-fw"></i><i class="fa fa-caret-down"></i></a>
-    <ul>
+    <a href="#" class="dropdown-menu dropdown-menu-link-icon">
+        <i class="fa fa-cog fa-fw"></i><i class="fa fa-caret-down"></i>
+    </a>
+    <ul class="">
         <?php if ($more): ?>
-        <li>
-            <i class="fa  fa-book" aria-hidden="true"></i>
-            <?= $this->modal->medium('', t('additional'), 'ContactsController', 'details', array('plugin' => 'AddressBook', 'project_id' => $project['id'], 'contacts_id' => $contacts_id)) ?>
-        </li>
+            <li class="">
+                <i class="fa  fa-book" aria-hidden="true"></i>
+                <?= $this->modal->medium('', t('additional'), 'ContactsController', 'details', array('plugin' => 'AddressBook', 'project_id' => $project['id'], 'contacts_id' => $contacts_id)) ?>
+            </li>
         <?php endif ?>
-        <li>
+        <li class="">
             <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
             <?= $this->modal->medium('', t('Edit'), 'ContactsController', 'edit', array('plugin' => 'AddressBook', 'project_id' => $project['id'], 'contacts_id' => $contacts_id)) ?>
         </li>
-        <li>
+        <li class="">
             <i class="fa fa-trash-o" aria-hidden="true"></i>
             <?= $this->modal->medium('', t('Remove'), 'ContactsController', 'confirm', array('plugin' => 'AddressBook', 'project_id' => $project['id'], 'contacts_id' => $contacts_id)) ?>
         </li>
