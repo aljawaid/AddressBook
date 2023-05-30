835f173cfc7ccaca91a0b4ebd025ba315ff25622
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-05-30 20:01:38 +0100

    Fix MySQL Schema

--- Schema/Mysql.php
+++ Schema/Mysql.php
@@ -8,8 +8,7 @@ const VERSION = 1;
 
 function version_1(PDO $pdo)
 {
-    // CREATE DYNAMIC ITEMS FOR ONE GENERAL CONTACT
-    // ITEM TYPE WILL BE VALIDATED IN FRONTEND
+    /* CREATE DYNAMIC ITEMS FOR ONE GENERAL CONTACT - ITEM TYPE WILL BE VALIDATED IN FRONTEND */
     $pdo->exec('CREATE TABLE IF NOT EXISTS address_book_contacts_items (
         `id` INT NOT NULL AUTO_INCREMENT,
         `item` VARCHAR(30) NOT NULL,
@@ -18,8 +17,8 @@ function version_1(PDO $pdo)
         PRIMARY KEY(id)
     ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
 
-    // SAVE VALUES OF DYNAMIC ITEMS FOR THE CONTACT
-    $pdo->exec("CREATE TABLE IF NOT EXISTS address_book_contacts_contact (
+    /* SAVE VALUES OF DYNAMIC ITEMS FOR THE CONTACT */
+    $pdo->exec('CREATE TABLE IF NOT EXISTS address_book_contacts_contact (
         `contacts_id` INT NOT NULL,
         `item_id` INT NOT NULL,
         `contact_item_value` TEXT NOT NULL,
@@ -28,19 +27,19 @@ function version_1(PDO $pdo)
         `last_updated` INTEGER NOT NULL,
         FOREIGN KEY(item_id) REFERENCES address_book_contacts_items(id) ON DELETE CASCADE,
         KEY(contacts_id)
-    ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci");
+    ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
 
-    // CREATE LINKS TO TASKS WITH CONTACTS
-    $pdo->exec("CREATE TABLE IF NOT EXISTS address_book_contacts_task_has_contact (
+    /* CREATE LINKS TO TASKS WITH CONTACTS */
+    $pdo->exec('CREATE TABLE IF NOT EXISTS address_book_contacts_task_has_contact (
         `id` INT NOT NULL AUTO_INCREMENT,
         `task_id` INT NOT NULL,
         `contacts_id` INT NOT NULL,
         FOREIGN KEY(contacts_id) REFERENCES address_book_contacts_contact(contacts_id) ON DELETE CASCADE,
         FOREIGN KEY(task_id) REFERENCES tasks(id) ON DELETE CASCADE,
         PRIMARY KEY(id)
-    ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci");
+    ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci');
 
-    // ADD DEFAULTS FOR CONTACT ITEMS
+    /* ADD DEFAULTS FOR CONTACT ITEMS */
     $pdo->exec('INSERT INTO `address_book_contacts_items`(`item`, `item_type`, `position`) VALUES ("Name", "text", 1)');
     $pdo->exec('INSERT INTO `address_book_contacts_items`(`item`, `item_type`, `position`) VALUES ("Organisation", "text", 2)');
     $pdo->exec('INSERT INTO `address_book_contacts_items`(`item`, `item_type`, `position`) VALUES ("Address", "address", 3)');
