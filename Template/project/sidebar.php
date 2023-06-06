4f10ba8e5395dc21397831821b19bca0275dbf6e
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-06 07:33:35 +0100

    Fix Sidebars

--- Template/project/sidebar.php
+++ Template/project/sidebar.php
@@ -1,5 +1,5 @@
-<?php if ($this->user->hasProjectAccess('contacts', 'index', $project['id'])): ?>
-    <li <?= $this->app->checkMenuSelection('ContactsController', 'project') ?>>
-        <?= $this->url->link(t('Address Book'), 'ContactsController', 'project', array('plugin' => 'AddressBook', 'project_id' => $project['id'])) ?>
+<?php if ($this->user->hasProjectAccess('ProjectViewController', 'show', $project['id'])): ?>
+    <li <?= $this->app->checkMenuSelection('ContactsController', 'project', 'AddressBook') ?>>
+        <?= $this->url->link(t('Address Book'), 'ContactsController', 'project', array('project_id' => $project['id'], 'plugin' => 'AddressBook')) ?>
     </li>
 <?php endif ?>
--- Template/task/sidebar.php
+++ Template/task/sidebar.php
@@ -1,6 +1,6 @@
 <?php if ($this->user->hasProjectAccess('contacts', 'index', $task['project_id'])): ?>
     <li class="">
         <i class="fa fa-phone fa-fw"></i>
-        <?= $this->url->link(t('Address Book'), 'ContactsController', 'task', array('plugin' => 'AddressBook', 'project_id' => $task['project_id'], 'task_id' => $task['id'])) ?>
+        <?= $this->url->link(t('Contacts'), 'ContactsController', 'task', array('project_id' => $task['project_id'], 'task_id' => $task['id'], 'plugin' => 'AddressBook')) ?>
     </li>
 <?php endif ?>
