c11a4e74a245fe11b5b228b5bf218b5773a5a6f7
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-06 19:51:11 +0100

    Fix Icons

--- Template/task/footer_icon.php
+++ Template/task/footer_icon.php
@@ -1,6 +1,6 @@
 <?php $contacts = $this->ContactsHelper->getContactsIDs($task['id']); ?>
 <?php if (!empty($contacts)): ?>
-    <span title="<?= t('Contacts') ?>" class="tooltip" data-href="<?= $this->url->href('ContactsController', 'task_footer', array('plugin' => 'AddressBook', 'task_id' => $task['id'])) ?>">
-        <i class="fa fa-phone fa-fw"></i>
+    <span title="<?= t('Contacts') ?>" class="tooltip" data-href="<?= $this->url->href('ContactsController', 'task_footer', array('task_id' => $task['id'], 'plugin' => 'AddressBook')) ?>">
+        <span class="address-book-icon"></span>
     </span>
 <?php endif ?>
--- Template/task/sidebar.php
+++ Template/task/sidebar.php
@@ -1,6 +1,6 @@
 <?php if ($this->user->hasProjectAccess('contacts', 'index', $task['project_id'])): ?>
     <li class="">
-        <i class="fa fa-phone fa-fw"></i>
+        <span class="address-book-icon"></span>
         <?= $this->url->link(t('Contacts'), 'ContactsController', 'task', array('project_id' => $task['project_id'], 'task_id' => $task['id'], 'plugin' => 'AddressBook')) ?>
     </li>
 <?php endif ?>
