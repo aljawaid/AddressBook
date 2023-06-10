bbc687021c832f6f3eb40d925d78e4885af24550
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-10 06:00:42 +0100

    Fix Plugin Description

--- Plugin.php
+++ Plugin.php
@@ -73,7 +73,7 @@ class Plugin extends Base
 
     public function getPluginDescription()
     {
-        return t('Use the new Address Book to create and manage contacts associating them with projects and tasks. Add custom properties per contact to log a relationship between tasks and people or organisations. Each task can have contacts assigned through a list of contacts exclusive to the project. Users can sort their contact properties to show the top 3 properties (e.g. name, number and email) so they can easily have a quick reference to contact information from the task summary.');
+        return t('Use the new Address Book to create and manage contacts associating them with projects and tasks. Add custom properties per contact to log a relationship between tasks and people or organisations. Each task can have contacts assigned through a list of contacts exclusive to the project. Users can sort their contact properties to show the first 3 properties (e.g. name, number and email) so they can easily have a quick reference to contact information from the task summary.');
     }
 
     public function getPluginAuthor()
