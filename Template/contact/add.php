b79ae057ca2943aac798ce1e495e8d310e44b34d
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-12 01:00:04 +0100

    Update Page HTML & Structure

--- Template/contact/add.php
+++ Template/contact/add.php
@@ -1,13 +1,12 @@
-<div class="">
-    <div class="page-header">
-        <h2 class=""><?= t('Add a new contact') ?></h2>
-    </div>
-    <form method="post" action="<?= $this->url->href('ContactsController', 'save', array('plugin' => 'AddressBook', 'project_id' => $project_id)) ?>" autocomplete="off">
+<div class="add-contact-wrapper">
+    <fieldset class="add-contact-profile">
+        <legend class=""><?= t('Add New Contact') ?></legend>
+        <form method="post" action="<?= $this->url->href('ContactsController', 'save', array('project_id' => $project_id, 'plugin' => 'AddressBook')) ?>" class="add-contact-form" autocomplete="on">
+            <?= $this->form->csrf() ?>
 
-        <?= $this->form->csrf() ?>
-        <?php foreach ($items as $key => $value): ?>
-            <?= $this->form->label($value['item'], $value['item']) ?>
-            <?= $this->form->text($value['id'] . '_' . $value['item'], $values, $errors, array('maxlength="100"')) ?>
+            <?php foreach ($items as $key => $value): ?>
+                <?= $this->form->label($value['item'], $value['id'] . '_' . strtolower($value['item']), array('class="profile-property-label"')) ?>
+                <?= $this->form->text($value['id'] . '_' . strtolower($value['item']), $values, $errors, array('maxlength="100"', 'placeholder="' . $value['item_type'] . '"'), 'property-input') ?>
                 <?php if (isset($value['item_help'])): ?>
                     <p class="form-help">
                         <?= $value['item_help'] ?>
@@ -15,8 +14,9 @@
                 <?php endif ?>
             <?php endforeach ?>
 
-        <div class="form-actions">
-            <button type="submit" class="btn btn-blue"><?= t('Save') ?></button>
-        </div>
-    </form>
+            <div class="form-actions">
+                <button type="submit" class="btn btn-blue"><?= t('Add Contact') ?></button>
+            </div>
+        </form>
+    </fieldset>
 </div>
