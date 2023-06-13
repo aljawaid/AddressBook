18637c24cce9a4e1d946df31d0502650b002029e
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-13 10:53:07 +0100

    Add Icon to Form Title

--- Template/contact/add.php
+++ Template/contact/add.php
@@ -1,6 +1,6 @@
 <div class="add-contact-wrapper">
     <fieldset class="add-contact-profile relative">
-        <legend class=""><?= t('Add New Contact') ?></legend>
+        <legend class=""><span class="add-contact-icon"></span><?= t('Add New Contact') ?></legend>
         <span class="form-help"><?= t('All fields are optional') ?></span>
         <form method="post" action="<?= $this->url->href('ContactsController', 'save', array('project_id' => $project_id, 'plugin' => 'AddressBook')) ?>" class="add-contact-form" autocomplete="on">
             <?= $this->form->csrf() ?>
