512caa43239f3850287249d6bafbc25bc8f3c711
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-13 15:18:35 +0100

    Restructure Delete Contact Modal

--- Template/contact/remove.php
+++ Template/contact/remove.php
@@ -1,16 +1,15 @@
-<section id="main">
+<div class="">
     <div class="page-header">
         <h2 class=""><?= t('Remove Contact') ?></h2>
     </div>
     <div class="confirm">
-        <p class="alert alert-info">
-            <?= t('Do you really want to remove this contact: "%s"?', $contact['value']) ?>
+        <p class="confirm-delete">
+            <?= e('Do you really want to remove %s as a contact in the Address Book?', '<strong>' . $contact['value'] . '</strong>') ?>
         </p>
 
         <div class="form-actions">
-            <?= $this->url->link(t('Yes'), 'ContactsController', 'remove', array('plugin' => 'AddressBook', 'project_id' => $project['id'], 'contacts_id' => $contacts_id), true, 'btn btn-red') ?>
-            <?= t('or') ?>
-            <?= $this->url->link(t('cancel'), 'ContactsController', 'project', array('plugin' => 'AddressBook', 'project_id' => $project['id']), false, 'close-popover') ?>
+            <?= $this->url->link(t('Delete'), 'ContactsController', 'remove', array('project_id' => $project['id'], 'contacts_id' => $contacts_id, 'plugin' => 'AddressBook'), true, 'btn btn-red') ?>
+            <button class="btn cancel-btn js-modal-close" href="#"><?= t('Cancel') ?></button>
         </div>
     </div>
-</section>
+</div>
