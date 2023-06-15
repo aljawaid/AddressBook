5e3f04a9c50387214b3ab513f79cc1cc4e5ae428
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-15 11:28:44 +0100

    Fix Modal Close Button Styling

--- Template/contact/details.php
+++ Template/contact/details.php
@@ -1,3 +1,37 @@
+<style type="text/css">
+    /* MODAL SIZE */
+    #modal-box {
+        width: auto !important;
+    }
+
+    #modal-content {
+        padding: 10px 15px;
+    }
+
+    /* MODAL CLOSE BUTTON */
+    #modal-close-button {
+        transform: scale(1.5);
+        display: inline-block;
+        position: absolute;
+        right: 6px;
+        top: 6px;
+        background: var(--pp-red-alt-2);
+        padding: 3px 3px 5px 6px;
+        border-bottom-left-radius: 3px;
+        box-shadow: -1px -1px 0px 3px var(--pp-white);
+    }
+
+    #modal-close-button i {
+        color: var(--pp-white);
+        transition: var(--transition-address-book);
+    }
+
+    #modal-close-button:hover i {
+        color: var(--pp-grey);
+        text-shadow: 0 0 1px var(--pp-white);
+    }
+</style>
+
 <div class="">
     <div class="page-header">
         <h2 class=""><?= t('Contact details') ?></h2>
