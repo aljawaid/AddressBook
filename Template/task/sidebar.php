53dfd8082ad7758c7e5e8143db731b4514b6197f
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-15 22:34:11 +0100

    Fix Sidebar Menu Selection

--- Template/task/sidebar.php
+++ Template/task/sidebar.php
@@ -1,5 +1,5 @@
 <?php if ($this->user->hasProjectAccess('contacts', 'index', $task['project_id'])): ?>
-    <li class="">
+    <li <?= $this->app->checkMenuSelection('ContactsController', 'task') ?>>
         <span class="address-book-icon"></span>
         <?= $this->url->link(t('Contacts'), 'ContactsController', 'task', array('project_id' => $task['project_id'], 'task_id' => $task['id'], 'plugin' => 'AddressBook')) ?>
     </li>
