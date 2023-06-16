6e507e940571214a112a5a98f226895e52f19251
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-17 00:12:14 +0100

    Manually Build Modals, Remove Dropdown Menu

--- Template/project/contacts.php
+++ Template/project/contacts.php
@@ -18,14 +18,14 @@
     </p>
     <?php if (!empty($contacts)): ?>
         <section class="project-contacts-section">
-            <table class="project-contacts-table table-small table-fixed">
+            <table id="ProjectContactsTable" class="project-contacts-table table-small table-fixed">
                 <thead class="">
                     <tr class="">
                         <th class="contacts-table-header column-20"><?= (empty($items[0])) ? "" : $items[0]['item'] ?></th>
                         <th class="contacts-table-header column-20"><?= (empty($items[1])) ? "" : $items[1]['item'] ?></th>
                         <th class="contacts-table-header column-20"><?= (empty($items[2])) ? "" : $items[2]['item'] ?></th>
                         <th class="contacts-table-header column-20"><?= (empty($items[3])) ? "" : $items[3]['item'] ?></th>
-                        <th class="contacts-table-header column-20"><?= t('Action') ?></th>
+                        <th class="contacts-table-header column-21"><?= t('Actions') ?></th>
                     </tr>
                 </thead>
                 <tbody class="">
@@ -45,7 +45,33 @@
                                 <?= (empty($values[4])) ? "" : $values[4]['contact_item_value'] ?>
                             </td>
                             <td class="contacts-table-value">
-                                <?= $this->render('addressBook:project/menu', array('project' => $project, 'more' => !empty($values[5]), 'contacts_id' => $value['contacts_id'])) ?>
+                                <ul class="contacts-action-btns">
+                                    <li class="">
+                                        <?php if (!empty($values[5])): ?>
+                                            <!-- Modal Button -->
+                                            <a href="<?= $this->url->href('ContactsController', 'details', array('contacts_id' => $value['contacts_id'], 'plugin' => 'AddressBook'), false, '', false) ?>" class="btn js-modal-medium view-contact-btn" title="<?= t('View more information for this contact') ?>">
+                                                <span class="contact-profile-icon"></span>
+                                            </a>
+                                        <?php else: ?>
+                                            <!-- Modal Button -->
+                                                <a href="<?= $this->url->href('ContactsController', 'details', array('contacts_id' => $value['contacts_id'], 'plugin' => 'AddressBook'), false, '', false) ?>" class="btn js-modal-medium view-contact-btn no-info" title="<?= t('View Contact') ?>">
+                                                    <span class="contact-profile-icon"></span>
+                                                </a>
+                                        <?php endif ?>
+                                    </li>
+                                    <li class="">
+                                        <!-- Modal Button -->
+                                        <a href="<?= $this->url->href('ContactsController', 'edit', array('project_id' => $project['id'], 'contacts_id' => $value['contacts_id'], 'plugin' => 'AddressBook'), false, '', false) ?>" class="btn btn-ab-rename js-modal-medium" title="<?=t('Edit Contact') ?>">
+                                            <span class="rename-icon"></span> <?= t('Edit') ?>
+                                        </a>
+                                    </li>
+                                    <li class="">
+                                        <a href="<?= $this->url->href('ContactsController', 'confirm', array('project_id' => $project['id'], 'contacts_id' => $value['contacts_id'], 'plugin' => 'AddressBook'), false, '', false) ?>" class="btn btn-ab-delete js-modal-medium" title="<?=t('Delete Contact') ?>">
+                                            <span class="delete-contact-icon"></span>
+                                            <span class="delete-text"><?= t('Delete') ?></span>
+                                        </a>
+                                    </li>
+                                </ul>
                             </td>
                         </tr>
                     <?php endforeach ?>
