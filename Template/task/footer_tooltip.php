04f7d04665c56b0b8f0fcec2f2debe53e2b90986
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-20 10:42:11 +0100

    Fix HTML Table Structure

--- Template/task/footer_tooltip.php
+++ Template/task/footer_tooltip.php
@@ -1,31 +1,33 @@
-<div class="">
-    <div class="page-header">
-        <h2 class=""><?= t('Task Contacts') ?></h2>
+<div class="footer-tooltip">
+    <div class="ab-page-header">
+        <h2 class=""><span class="linked-contact-icon"></span><?= t('Task Contacts') ?></h2>
     </div>
-
-    <?php if (empty($contacts)): ?>
-        <p class="alert"><?= t('No contacts') ?></p>
-    <?php else: ?>
+    <?php if (!empty($contacts)): ?>
         <?php $items = $this->ContactsHelper->getItems() ?>
-        <table class="table-small table-fixed">
-            <tr class="">
-                <th class="column-25"><?= (empty($items[0])) ? "" : $items[0]['item'] ?></th>
-                <th class="column-25"><?= (empty($items[1])) ? "" : $items[1]['item'] ?></th>
-                <th class="column-25"><?= (empty($items[2])) ? "" : $items[2]['item'] ?></th>
-                <th class="column-15"></th>
-            </tr>
-            <?php foreach ($contacts as $key => $value): ?>
-                <?php $values = $this->ContactsHelper->getContactByID($value['contacts_id']) ?>
-                <tr class="">
-                    <td class=""><?= (empty($values[1])) ? "" : $values[1]['contact_item_value'] ?></td>
-                    <td class=""><?= (empty($values[2])) ? "" : $values[2]['contact_item_value'] ?></td>
-                    <td class=""><?= (empty($values[3])) ? "" : $values[3]['contact_item_value'] ?></td>
-                    <td class="">
-                        <?php if (!empty($values[4])): ?>
-                            <?= $this->modal->medium('', t('View Contact'), 'ContactsController', 'details', array('contacts_id' => $value['contacts_id'], 'plugin' => 'AddressBook')) ?>
-                        <?php endif ?>
-                    </td>
+        <table id="TooltipContactsTable" class="table-small table-fixed">
+            <thead class="table-head">
+                <tr class="table-row">
+                    <th class="column-25"><?= (empty($items[0])) ? "" : $items[0]['item'] ?></th>
+                    <th class="column-25"><?= (empty($items[1])) ? "" : $items[1]['item'] ?></th>
+                    <th class="column-25"><?= (empty($items[2])) ? "" : $items[2]['item'] ?></th>
+                    <th class="column-15"></th>
                 </tr>
+            </thead>
+            <tbody class="table-body">
+                <?php foreach ($contacts as $key => $value): ?>
+                    <?php $values = $this->ContactsHelper->getContactByID($value['contacts_id']) ?>
+                    <tr class="table-row">
+                        <td class=""><?= (empty($values[1])) ? "" : $values[1]['contact_item_value'] ?></td>
+                        <td class=""><?= (empty($values[2])) ? "" : $values[2]['contact_item_value'] ?></td>
+                        <td class=""><?= (empty($values[3])) ? "" : $values[3]['contact_item_value'] ?></td>
+                        <td class="">
+                            <?php if (!empty($values[4])): ?>
+                                <?= $this->modal->medium('', t('View Contact'), 'ContactsController', 'details', array('contacts_id' => $value['contacts_id'], 'plugin' => 'AddressBook')) ?>
+                            <?php endif ?>
+                        </td>
+                    </tr>
+                </tbody>
             <?php endforeach ?>
         </table>
     <?php endif ?>
+</div>
