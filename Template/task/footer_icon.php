c4cd9a8c8809cdd1c29a45dfab3812d946ab96c1
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-20 11:02:11 +0100

    Style tooltip icon in board

--- Template/task/footer_icon.php
+++ Template/task/footer_icon.php
@@ -1,7 +1,8 @@
 <?php $contacts = $this->ContactsHelper->getContactsIDs($task['id']); ?>
 
 <?php if (!empty($contacts)): ?>
-    <div title="<?= t('Contacts') ?>" class="tooltip" data-href="<?= $this->url->href('ContactsController', 'boardTaskFooter', array('task_id' => $task['id'], 'plugin' => 'AddressBook')) ?>">
+    <!-- Leave title tag empty for js rendering -->
+    <span title="" class="footer-icon tooltip" data-href="<?= $this->url->href('ContactsController', 'boardTaskFooter', array('task_id' => $task['id'], 'plugin' => 'AddressBook')) ?>">
         <span class="address-book-icon"></span><?= count($contacts) ?>
-    </div>
+    </span>
 <?php endif ?>
