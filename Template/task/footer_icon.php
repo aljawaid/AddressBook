8301b729884e2fbfc725dab1278bfe75888296f2
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-20 10:42:33 +0100

    Add tooltip count

--- Template/task/footer_icon.php
+++ Template/task/footer_icon.php
@@ -1,7 +1,7 @@
 <?php $contacts = $this->ContactsHelper->getContactsIDs($task['id']); ?>
 
 <?php if (!empty($contacts)): ?>
-    <span title="<?= t('Contacts') ?>" class="tooltip" data-href="<?= $this->url->href('ContactsController', 'boardTaskFooter', array('task_id' => $task['id'], 'plugin' => 'AddressBook')) ?>">
-        <span class="address-book-icon"></span>
-    </span>
+    <div title="<?= t('Contacts') ?>" class="tooltip" data-href="<?= $this->url->href('ContactsController', 'boardTaskFooter', array('task_id' => $task['id'], 'plugin' => 'AddressBook')) ?>">
+        <span class="address-book-icon"></span><?= count($contacts) ?>
+    </div>
 <?php endif ?>
