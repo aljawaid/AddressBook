ae109d7f8373c153140119b90ab64ef611c35c99
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-21 09:18:43 +0100

    Display Property Set - Template - Team

--- Template/config/property-sets.php
+++ Template/config/property-sets.php
@@ -55,4 +55,16 @@
             <li class=""><?= t('Mobile') ?></li>
         </ul>
     </section>
+    <section class="property-set-section">
+        <a href="<?= $this->url->href('ContactsItemsController', 'insertSetTeam', array('plugin' => 'AddressBook')) ?>" class="btn btn-ab-move add-property-set" title="<?= t('Add Set') ?>"><i class="fa fa-plus" aria-hidden="true"></i></a>
+        <a href="<?= $this->url->href('ContactsItemsController', 'removeSetTeam', array('plugin' => 'AddressBook')) ?>" class="btn btn-ab-move remove-property-set" title="<?= t('Remove Set') ?>"><i class="fa fa-minus" aria-hidden="true"></i></a>
+        <div class="property-set-name"><?= t('Team') ?></div>
+        <ul class="property-set-items">
+            <li class=""><?= t('Title') ?></li>
+            <li class=""><?= t('Telephone') ?></li>
+            <li class=""><?= t('Mobile') ?></li>
+            <li class=""><?= t('Email') ?></li>
+            <li class=""><?= t('Note') ?></li>
+        </ul>
+    </section>
 </div>
