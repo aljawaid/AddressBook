672352878dfcd5af17f517aef55f36ce11af2ab4
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-23 18:34:45 +0100

    Delete project/index.php

--- Template/project/index.php
+++
@@ -1,2 +0,0 @@
-<?= $this->render('custom_filter/add', array('project' => $project, 'values' => $values, 'errors' => $errors)) ?>
-<?= $this->render('custom_filter/add', array('project' => $project, 'values' => $values, 'errors' => $errors)) ?>
