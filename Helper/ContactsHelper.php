33a36a9ac69c985e6ae8f73a5e941e0637f8dec0
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-27 00:23:03 +0100

    Add class to modal label

--- Helper/ContactsHelper.php
+++ Helper/ContactsHelper.php
@@ -16,9 +16,9 @@ class ContactsHelper extends Base
     {
         $attributes = array_merge(array('tabindex="1"', 'required', 'maxlength="30"'), $attributes);
 
-        $html = $this->helper->form->label(t('Property Name'), 'item');
+        $html = $this->helper->form->label(t('Property Name'), 'item', array('class="profile-property-label"'));
         $html .= $this->helper->form->text('item', $values, $errors, $attributes);
-        $html .= $this->helper->form->label(t('Property Note'), 'item_help');
+        $html .= $this->helper->form->label(t('Property Note'), 'item_help', array('class="profile-property-label"'));
         $html .= $this->helper->form->text('item_help', $values, $errors, array('tabindex="2"', 'maxlength="100"', 'placeholder="' . $values['item_help'] . '"', 'title="' . t('Maximum 100 characters only') . '"'));
         $html .= '<p class="form-help modal-form-help">' . t('Add a short descriptive note for this property to help users when creating contacts') . '</p>';
 
