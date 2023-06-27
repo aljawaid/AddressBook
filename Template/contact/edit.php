766fe1ecd7a58ea21e5d009911076e6fd71ae9ca
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-27 11:14:21 +0100

    Display Correct Fields Based on Property Types in Contact Edit Modal

--- Template/contact/edit.php
+++ Template/contact/edit.php
@@ -44,11 +44,34 @@
             <?php
             $trimmedItemName = ucwords(strtolower($value));
             $trimmedItem = str_replace(array(' ', '|', '(', ')', '[', ']'), '', $trimmedItemName);
+            $item_type = $this->ContactsHelper->getItemType($key);
             ?>
 
             <div class="form-group">
                 <?= $this->form->label($value, $key . '__' . $trimmedItem) ?>
-                <?= $this->form->text($key . '__' . $trimmedItem, $values, $errors, array('maxlength="100"', 'placeholder="' . $value . '"'), 'property-input') ?>
+                <?php if ($item_type == 'number'): ?>
+                    <?= $this->form->number($key . '__' . $trimmedItem, $values, $errors, array('placeholder="' . $value . '"', 'title="' . t('Format: 12345') . '"', 'step="1"'), 'property-input property-input-number') ?>
+                <?php elseif ($item_type == 'decimal2'): ?>
+                        <?= $this->form->input('number', $key . '__' . $trimmedItem, $values, $errors, array('placeholder="' . $value . '"', 'placeholder="' . t('Format: 0.01') . '"', 'step="0.01"'), 'property-input property-input-decimal') ?>
+                <?php elseif ($item_type == 'decimal4'): ?>
+                        <?= $this->form->input('number', $key . '__' . $trimmedItem, $values, $errors, array('placeholder="' . $value . '"', 'title="' . t('Format: 0.0001') . '"', 'step="0.0001"'), 'property-input property-input-decimal') ?>
+                <?php elseif ($item_type == 'telephone'): ?>
+                    <?= $this->form->input('tel', $key . '__' . $trimmedItem, $values, $errors, array('placeholder="' . $value . '"', 'title="' . t('Format: +44 (0)1234 567890') .'"', 'pattern="^[0-9-+\s()]*$"'), 'property-input property-input-telephone') ?>
+                <?php elseif ($item_type == 'email'): ?>
+                    <?= $this->form->email($key . '__' . $trimmedItem, $values, $errors, array('placeholder="' . $value . '"', 'title="' . t('Format: someone@somewhere.com') . '"'), 'property-input') ?>
+                <?php elseif ($item_type == 'url'): ?>
+                    <?= $this->form->input('url', $key . '__' . $trimmedItem, $values, $errors, array('placeholder="' . $value . '"', 'title="' . t('Format: https://') . '"'), 'property-input') ?>
+                <?php elseif ($item_type == 'textarea'): ?>
+                    <?= $this->form->textarea($key . '__' . $trimmedItem, $values, $errors, array('placeholder="' . $value . '"', 'title="' . t('Save notes for this contact') . '"', 'rows="3"', 'cols="37"', 'wrap="hard"'), 'property-input-note') ?>
+                <?php elseif ($item_type == 'address'): ?>
+                    <?= $this->form->textarea($key . '__' . $trimmedItem, $values, $errors, array('placeholder="' . $value . '"', 'title="' . t('Enter address details for this contact') . '"', 'rows="3"', 'cols="37"', 'wrap="hard"'), 'property-input-address') ?>
+                <?php else: ?>
+                    <?php if ($key == 1): ?>
+                        <?= $this->form->text($key . '__' . $trimmedItem, $values, $errors, array('placeholder="' . $value . '"', 'maxlength="50"', 'autofocus', 'required'), 'property-input') ?>
+                    <?php else: ?>
+                            <?= $this->form->text($key . '__' . $trimmedItem, $values, $errors, array('placeholder="' . $value . '"', 'maxlength="50"'), 'property-input') ?>
+                    <?php endif ?>
+                <?php endif ?>
             </div>
         <?php endforeach ?>
 
