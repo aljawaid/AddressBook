d19553f3035668937d28f3e9e661b90f81f69c03
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-28 14:13:05 +0100

    Add & Style Sidebar Badge - task

--- Template/task/sidebar.php
+++ Template/task/sidebar.php
@@ -1,6 +1,15 @@
+<?php $linkedContacts = $this->model->contactsTaskModel->getLinked($task['id']); ?>
+
+<style type="text/css">
+    li {
+        position: relative;
+    }
+</style>
+
 <?php if ($this->user->hasProjectAccess('contacts', 'index', $task['project_id'])): ?>
     <li <?= $this->app->checkMenuSelection('ContactsController', 'task') ?>>
         <span class="address-book-icon"></span>
         <?= $this->url->link(t('Contacts'), 'ContactsController', 'task', array('project_id' => $task['project_id'], 'task_id' => $task['id'], 'plugin' => 'AddressBook')) ?>
+        <span class="ab-count-badge-sidebar linked-badge"><?= count($linkedContacts) ?></span>
     </li>
 <?php endif ?>
