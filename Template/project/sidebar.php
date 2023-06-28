956acfbd539d64d4a57617e515db54bfaddf5668
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-28 13:55:39 +0100

    Add & Style Sidebar Badge - project

--- Template/project/sidebar.php
+++ Template/project/sidebar.php
@@ -1,5 +1,14 @@
+<?php $projectContacts = $this->model->contactsModel->getAll(); ?>
+
+<style type="text/css">
+    li {
+        position: relative;
+    }
+</style>
+
 <?php if ($this->user->hasProjectAccess('ProjectViewController', 'show', $project['id'])): ?>
     <li <?= $this->app->checkMenuSelection('ContactsController', 'project', 'AddressBook') ?>>
         <?= $this->url->link(t('Address Book'), 'ContactsController', 'project', array('project_id' => $project['id'], 'plugin' => 'AddressBook')) ?>
+        <span class="ab-count-badge-sidebar"><?= count($projectContacts) ?></span>
     </li>
 <?php endif ?>
