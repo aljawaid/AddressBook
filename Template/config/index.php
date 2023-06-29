510965f656f2c8844f3a2c1588271e1a3ef77407
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-29 05:01:48 +0100

    Delete Icon
    - not really needed

--- Template/config/index.php
+++ Template/config/index.php
@@ -57,8 +57,8 @@
             <table id="ContactProfileTable" class="contact-profile-table">
                 <thead class="table-head">
                     <tr class="table-row">
-                        <th class="column-13"><span class="property-icon"></span> <?= t('Contact Property') ?></th>
                         <th class="column-25"><?= t('Property Note') ?></th>
+                        <th class="column-13"><?= t('Contact Property') ?></th>
                         <th class="column-8 text-center"><?= t('Property Type') ?></th>
                         <th class="column-8 text-center"><?= t('Property Set') ?></th>
                         <th class="column-20"><?= t('Actions') ?></th>
