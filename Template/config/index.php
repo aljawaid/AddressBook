b2bfc142257f80cf8c10cbb91e6998bb18168c54
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-06-29 05:03:19 +0100

    Fix Column Widths for `ContactProfileTable`

--- Template/config/index.php
+++ Template/config/index.php
@@ -57,11 +57,11 @@
             <table id="ContactProfileTable" class="contact-profile-table">
                 <thead class="table-head">
                     <tr class="table-row">
-                        <th class="column-25"><?= t('Property Note') ?></th>
-                        <th class="column-13"><?= t('Contact Property') ?></th>
+                        <th class=""><?= t('Contact Property') ?></th>
+                        <th class="column-10 property-note-column"><?= t('Property Note') ?></th>
                         <th class="column-8 text-center"><?= t('Property Type') ?></th>
                         <th class="column-8 text-center"><?= t('Property Set') ?></th>
-                        <th class="column-20"><?= t('Actions') ?></th>
+                        <th class="column-25"><?= t('Actions') ?></th>
                     </tr>
                 </thead>
                 <tbody class="table-body">
